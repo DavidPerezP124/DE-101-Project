2d44ca5d6d24bd97bdd684e5b7dcdccc1d09dc80
David Perez <davidpp124@gmail.com>
2023-04-24 12:28:55 -0600

    Update Solutions and queries

--- Solution.md
+++ Solution.md
@@ -1,10 +1,28 @@
-# Project Solution
+# PROJECT SOLUTION
 
-Analyze the data to figure out what we have in a notebook.
+## Processing
 
-Separate what is going to be our fact = sale
+The type of data pipeline will be ETL, since the source data comes from a non-normalized source which is the NikeScrAPI.
 
-And what are we going to use for our dimensions.
+### ETL
 
-Each row is already identified, it can be used for the sales fact.
+- Extraction:
+    This uses the NikeScrapi to scrape data from a Nike API endpoint, and saves it into a pandas data frame.
+    Use pandas to save into a csv with a pipe separator since CSV reading in later steps may be affected due to 
+    Short-Description having several commas inside its text. 
 
+- Transform:
+    This uses a SparkSubmitOperator to get the csv, remove some columns and then save into a local postgres DB.
+
+- Load:
+    This uses a SparkSubmitOperator to get the data from a local postgres DB and loads it into a Snowflake Warehouse.
+
+## Warehouse
+
+For a data Warehouse we are using Snowflake, and we already have a database, a schema and SQL worksheets to handle data for analysis.
+
+1. First step is to create the tables, this is done by running the statements in the table_creation.sql file.
+
+2. Second step is to insert the data from the raw table into our schema tables, this is done by running the statements in the table_insertion.sql file.
+
+3. Lastly, we query the elements we want to get for data visualization, with a view to make it easier to use in a BI instrument.
--- sql_queries.sql
+++ sql_queries.sql
@@ -1,18 +1,7 @@
 
 
 --1 Query the top 5 sales by product
---1.1 TOP 5 MOST SOLD PRODUCT
-SELECT
-    pd.title,
-    SUM(amount) as sales
-FROM sale_fact sf
-JOIN 
-    product_dim pd ON pd.id = sf.product_id
-GROUP BY  pd.title
-ORDER BY sales DESC
-LIMIT 5;
-
---1.2 EXTRA TOP 5 MOST REVENUE PER PRODUCT
+CREATE VIEW TOP_5_MOST_SALES_BY_PRODUCTS AS
 SELECT
     pd.title,
     SUM(total_sales) as sales
@@ -24,6 +13,7 @@ ORDER BY sales DESC
 LIMIT 5;
 
 -- Query the top 5 sales by category agrupation
+CREATE VIEW TOP_5_MOST_SALES_BY_CATEGORY AS
 SELECT
     cd.name,
     SUM(amount * total_sales) as sales
@@ -36,6 +26,7 @@ LIMIT 5;
 
 
 -- Query the least 5 sales by category agrupation
+CREATE VIEW TOP_5_LEAST_SALES_BY_CATEGORY AS
 SELECT
     cd.name,
     SUM(amount * total_sales) as sales
@@ -47,6 +38,7 @@ ORDER BY sales
 LIMIT 5;
 
 -- Query the top 5 sales by title and subtitle agrupation
+CREATE VIEW TOP_5_MOST_SALES_BY_TITLE AS
 SELECT
     pd.title,
     pd.subtitle,
@@ -54,11 +46,12 @@ SELECT
 FROM sale_fact sf
 JOIN 
     product_dim pd ON pd.id = sf.product_id
-GROUP BY  pd.title, pd.subtitle
+GROUP BY pd.title, pd.subtitle
 ORDER BY sales DESC
 LIMIT 5;
 
 -- Query the top 3 products that has greatest sales by category
+CREATE VIEW TOP_5_MOST_SALES_BY_CATEGORY_PRODUCT AS
 WITH ranked_cte AS (
     SELECT
         pd.title as title,
@@ -70,28 +63,6 @@ WITH ranked_cte AS (
         product_dim pd ON pd.id = sf.product_id
     JOIN 
         category_dim cd ON cd.id = sf.category_id
-    GROUP BY 
-        title,category,sf.category_id
-)
-SELECT
-    title, category, sales
-FROM ranked_cte
-WHERE ranking = 1
-ORDER BY sales DESC
-LIMIT 3;
-
--- EXTRA: BEST SELLING COLOR PER PRODUCT
-WITH ranked_cte AS (
-    SELECT
-        pd.title as title,
-        cd.id as color,
-        SUM(amount  * total_sales) as sales, 
-        RANK() OVER (PARTITION BY sf.color_id ORDER BY sales DESC) as ranking
-    FROM sale_fact sf
-    JOIN 
-        product_dim pd ON pd.id = sf.product_id
-    JOIN 
-        color_dim cd ON cd.id = sf.color_id
     GROUP BY 
         title,category,sf.category_id
 )
